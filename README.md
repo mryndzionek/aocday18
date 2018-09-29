880e470eb0b78cbbc38577b45d4f174740f30345
Mariusz Ryndzionek <mryndzionek@gmail.com>
2018-09-29 17:10:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Advent of Code 2017 - Day 18 in Haskell
+
+Solution to problem from day 18 of [Advent of Code 2017](https://adventofcode.com/2017/day/18).
+This is basically a simualtion of two imperative programs interacting via queue.
+What better problem to test the limits of pure FP :)
+
+Additional premises:
+
+- fairly typesafe
+- proper parser using [megaparsec](http://hackage.haskell.org/package/megaparsec)
+- accessors via [lenses](http://hackage.haskell.org/package/lens) 
---
+++ src/Main.hs
@@ -0,0 +1,171 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Main where
+import Data.Void
+import Data.Maybe
+import Control.Monad
+import Control.Monad.State
+import qualified Data.Sequence as Seq
+import qualified Data.Map.Strict as Map
+
+import System.Environment
+
+import Text.Megaparsec hiding (State, parse)
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as Lx
+import Control.Lens hiding (Const)
+
+type Reg = Char
+type Val = Integer
+data Var = Reg Reg | Const Val deriving (Show)
+
+data Cmd = Snd Reg |
+           Set Reg Var |
+           Add Reg Var |
+           Mul Reg Var |
+           Mod Reg Var |
+           Rcv Reg |
+           Jgz Var Var deriving (Show)
+
+type Parser = Parsec Void String
+
+type Env = Map.Map Reg Val
+type Program = [Cmd]
+
+data St = St {
+  _env  :: Env,
+  _pc   :: Int,
+  _inp  :: Seq.Seq Integer,
+  _out  :: Seq.Seq Integer,
+  _sc   :: Integer
+} deriving (Show)
+
+makeLenses ''St
+
+-- Program parser -------------------------------------
+lexeme :: Parser a -> Parser a
+lexeme = Lx.lexeme space
+
+symbol :: String -> Parser String
+symbol = Lx.symbol space1
+
+letter :: Parser Char
+letter = lexeme letterChar
+
+integer :: Parser Integer
+integer = lexeme Lx.decimal
+
+signedInteger :: Parser Integer
+signedInteger = Lx.signed space integer
+
+var :: Parser Var
+var = reg <|> cnst
+  where
+    reg = letter >>= \r -> return $ Reg r
+    cnst = signedInteger >>= \c -> return $ Const c
+
+cmd :: Parser Cmd
+cmd = singleArg <|> twoArgs <|> jgZ
+  where
+    singleArg = foldl1 (<|>) $ map si [("rcv", Rcv), ("snd", Snd)]
+    si :: (String, Reg -> Cmd) -> Parser Cmd
+    si (s, t) = do
+      _ <- symbol s
+      r <- letter
+      return $ t r
+    twoArgs = foldl1 (<|>) $ map ta [("set", Set), ("add", Add), ("mul", Mul), ("mod", Mod)]
+    ta :: (String, Reg -> Var -> Cmd) -> Parser Cmd
+    ta (s, t) = do
+      _ <- symbol s
+      r <- letter
+      v <- var
+      return $ t r v
+    jgZ = do
+      _ <- symbol "jgz"
+      v' <- var
+      v  <- var
+      return $ Jgz v' v 
+
+parse :: String -> Either (ParseError Char Void) [Cmd]
+parse input = mapM (runParser cmd "") (lines input)
+-------------------------------------------------------
+
+safeLu :: Int -> [a] -> Maybe a
+safeLu i a
+  | (i >= 0) && (length a > i) = Just (a !! i)
+  | otherwise = Nothing
+
+eval_ :: Var -> State St Val
+eval_ v = case v of
+  Const c -> return c
+  Reg   r -> (fromMaybe 0 . Map.lookup r . view env) <$> get
+
+set_ :: Reg -> Var -> State St ()
+set_ r v = eval_ v >>= modify . over env . Map.insert r
+
+process :: Cmd -> State St ()
+process c = case c of
+  Set r v  -> set_ r v
+  Add r v  -> op_ r v (+)
+  Mul r v  -> op_ r v (*)
+  Mod r v  -> op_ r v (flip rem)
+  Jgz v' v -> eval_ v' >>= (\a -> when (a > 0) $ jmp v)
+  Snd r    -> eval_ (Reg r) >>= (\a -> modify $ (out %~ (a Seq.<|)) . (sc %~ (+1)))
+  _ -> return ()
+  where
+    op_ r v f = eval_ v >>= (\a -> modify $ over env (adj (f a) r))
+    jmp v = eval_ v >>= (\a -> modify $ over pc ((fromIntegral a - 1) +))
+    adj f' k m = case Map.lookup k m of
+      Nothing -> Map.insert k (f' 0) m
+      Just _  -> Map.adjust f' k m
+
+interpret :: Program -> State St ()
+interpret p = intpr
+  where
+    intpr = do
+      s <- get
+      case safeLu (_pc s) p of
+        Nothing  -> return ()
+        Just c -> do
+          process c
+          s' <- get
+          case c of
+            Rcv r ->
+              if Seq.null (view inp s') then return () else
+              do let (inp_ Seq.:|> v) = s ^. inp
+                 set_ r (Const v)
+                 modify $ inp .~ inp_
+                 modify $ pc %~ (+ 1)
+                 intpr
+            _ -> do
+              modify $ pc %~ (+ 1)
+              intpr
+
+runPrgm :: Program -> St -> Seq.Seq Integer -> Seq.Seq Integer
+runPrgm p s i = let s' = runState (interpret p) (set inp i s)
+                in snd s' ^. out
+
+runPrgms :: Program -> St -> St -> (St, Seq.Seq Integer, St, Seq.Seq Integer)
+runPrgms p s1 s2 = until (\(_, ia, _, ib) -> Seq.null ia && Seq.null ib) run (s1', i1', s2', i2')
+  where
+    (s1', i1', s2', i2') = run (s1, Seq.empty, s2, Seq.empty)
+    run (sa, ia, sb, ib) = let sa' = runState (interpret p) (sa & inp .~ ia & out .~ Seq.empty)
+                               sb' = runState (interpret p) (sb & inp .~ ib & out .~ Seq.empty)
+                           in (snd sa', sb' ^. (_2 . out), snd sb', sa' ^. (_2 . out))
+      
+main :: IO ()
+main = do
+  args <- getArgs
+  i <- readFile $ head args
+  let prgm = parse i
+  case prgm of
+    Right p -> do
+      let op1 = runPrgm p initS Seq.empty
+      let (v Seq.:<| _) = op1
+      putStrLn $ "Part 1: " ++ show v
+      let op2 = runPrgms p (St (Map.fromList [('p', 0)]) 0 Seq.empty Seq.empty 0)
+                           (St (Map.fromList [('p', 1)]) 0 Seq.empty Seq.empty 0)
+      putStrLn $ "Part 2: " ++ show (op2 ^. _3 ^. sc)
+    Left e -> putStrLn $ "Error: " ++ show e
+    where
+      initS = St Map.empty 0 Seq.empty Seq.empty 0
